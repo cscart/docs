1ac34fb40b4ab059006b5fe0a2f2add23b856368
Konstantin Molchanov <kmolchanov@cs-cart.com>
2013-09-27 18:03:37 +0400

    Copyright set to Simtech

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'CS-Cart'
-copyright = '2013, Konstantin Molchanov'
+copyright = '2013, Simtech'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
