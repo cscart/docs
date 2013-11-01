b2f357f1e92e6b7a3433dae5a52d57a9eb289396
Konstantin Molchanov <kmolchanov@cs-cart.com>
2013-11-01 15:20:43 +0400

    Fancybox added (installed from git temporarily)

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 #extensions = ['sphinxcontrib.fancybox']
-extensions = []
+extensions = ['sphinxcontrib.fancybox']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -294,3 +294,7 @@ from pygments.lexers.web import PhpLexer
 lexers['phpinline'] = PhpLexer(startinline=True)
 
 highlight_language = 'phpinline'
+
+# FancyBox defaults
+fancybox_thumbnail_width = '100%'
+fancybox_thumbnail_height = '100%'
