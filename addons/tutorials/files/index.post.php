3869232219d8ee3c14a222966de07dcc34eebbac
Konstantin Molchanov <kmolchanov@cs-cart.com>
2013-12-18 19:18:20 +0400

    Addons: Tutorials: Advanced: Typos fixed.

--- addons/tutorials/files/index.post.php
+++ addons/tutorials/files/index.post.php
@@ -11,21 +11,21 @@
 * PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
 * "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
 ****************************************************************************/
- 
+
 if ( !defined('AREA') ) { die('Access denied'); }
- 
-$viewed_categories = db_get_array('SELECT * FROM ?:test_addon_data');
- 
+
+$viewed_categories = db_get_array('SELECT * FROM ?:advanced_addon_data');
+
 if (!empty($viewed_categories)) {
 	foreach ($viewed_categories as $key => $category_data) {
 		$category_data['user_name'] = fn_get_user_name($category_data['user_id']);
 		$category_data['categories'] = unserialize($category_data['categories']);
 		$category_data['categories']  = fn_get_category_name(array_keys($category_data['categories']));
- 
+
 		$viewed_categories[$key] = $category_data;
 	}
- 
+
 	Registry::get('view')->assign('viewed_categories', $viewed_categories);
 }
- 
+
 ?>
