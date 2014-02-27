2e394c353e47a416577c92f9bd240e05f35a1965
Konstantin Molchanov <moigagoo@live.com>
2014-02-27 11:42:15 +0400

    Logo moved from _img to _static.

--- conf.py
+++ conf.py
@@ -112,7 +112,7 @@ html_theme_path = ['_themes']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_img/cscart_logo.png'
+html_logo = '_static/cscart_logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -191,7 +191,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '_img/cscart.png'
+latex_logo = '_static/cscart.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
