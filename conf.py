1944adbc90a991ed7243ac77ab46d4acb6b7a3f3
Alexander Bolshakov <abolshakov@cs-cart.com>
2016-03-25 15:09:47 +0300

    [~] {@16293} Docs: copyright updated.

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'CS-Cart'
-copyright = '2015, Simtech'
+copyright = '2016, Simtech'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
