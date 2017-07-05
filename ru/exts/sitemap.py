5fe926634e451bb2001982d1dd41f5fe3e16728d
Valery Malyshev <vmalyshev@wolf10.ru>
2017-07-05 13:15:04 +0400

    [~] {@18884} Generate sitemap.txt was corrected

--- ru/exts/sitemap.py
+++ ru/exts/sitemap.py
@@ -8,8 +8,11 @@ def setup(app):
 def envpurgedoc(app, env, docnames):
     f = open(join(app.outdir, 'sitemap.txt'), 'w')
 
+    split_index = app.outdir.rfind('/')
+    version = app.outdir[split_index + 1:]
+
     for docname in docnames:
-        url = "https://www.cs-cart.ru/docs/4.6.x/" + docname + ".html"
+        url = join('https://www.cs-cart.ru/docs', version, docname + '.html')
         f.write(url + '\n')
 
     f.close()
