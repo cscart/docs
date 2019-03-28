a5ee4459d74df42188ce370ed29bb0c251d940d9
Ivan Koshkin <ikoshkin@cs-cart.com>
2019-03-28 09:41:24 +0400

    En&Ru: Tweaks to Upgrade Connector.

--- en/upgrade/files/Connector.php
+++ en/upgrade/files/Connector.php
@@ -57,7 +57,7 @@ class Connector extends BaseAddonConnector implements IConnector
         // add-on data
         $addon = SchemesManager::getScheme($this->addon_id);
 
-        $this->addon_version = isset($addon->version) ? $addon->version : '1.0';
+        $this->addon_version = $addon->getVersion() ? $addon->getVersion() : '1.0';
         // value of the 'license_number' of the add-on
         $this->license_number = (string) Settings::instance()->getValue('license_number', $this->addon_id);
 
