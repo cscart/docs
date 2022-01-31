18897bb8a730dffd4ffcca64a9e27fb7dc06633c
Alisa Maksina <amaksina@cs-cart.com>
2022-01-31 17:04:11 +0400

    Docs: copyright updated

--- en/conf.py
+++ en/conf.py
@@ -12,7 +12,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys, os, time
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'CS-Cart'
-copyright = '2013, Simtech'
+copyright = '{}, CS-Cart'.format(time.strftime('%Y'))
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
